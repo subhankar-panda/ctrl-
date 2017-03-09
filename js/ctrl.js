a5f455bb0699061480f6480c5fd36f931b7c321e
Subhankar <s1panda@ucsd.edu>
2017-03-08 16:29:20 -0800

    added more keys

--- js/ctrl.js
+++ js/ctrl.js
@@ -2,12 +2,21 @@ chrome.commands.onCommand.addListener(function (command) {
 	switch(command){
 		case "last_0":
 			goToTab(0);
+			break;
+		case "last_1":
+			goToTab(1);
+			break;
+		case "last_2":
+			goToTab(2);
+			break;
+		case "last_3":
+			goToTab(3);
+			break;
 	}
 });
 
 var goToTab = function(index){
 	chrome.tabs.query({}, function(result){
-		console.log(result);
 		chrome.tabs.update(result[result.length - 1 - index].id, {"active" : true})
 	});
 }
